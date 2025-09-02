2b283211f04c7ae8daaf65c19680f32456ae7e54
g3thsemane <saltyravensfan@gmail.com>
2025-09-02 16:33:48 -0400

    Modified Code Numbers
    Modified number in the code: made the circle blue, modified canvas size, modified pyramid, modified background

--- topics/version control/version-control-workflow/js/script.js
+++ topics/version control/version-control-workflow/js/script.js
@@ -14,23 +14,23 @@
 */
 function setup() {
     // A 640x480 canvas
-    createCanvas(640, 640);
+    createCanvas(650, 200);
 
     // Don't show the cursor
     noCursor();
 }
 
 /**
- * Draws a top-down view of a pyramid and also a red circle
+ * Draws a top-down view of a pyramid and also a green circle
  * at the position of the user's cursor
 */
 function draw() {
-    // Make the background black (specified as RGB)
-    background(0, 0, 0);
+    // Make the background red (specified as RGB)
+    background(0, 310, 255);
 
     // Draw a pyramid
     // How many levels for the pyramid
-    const levels = 10;
+    const levels = 25;
     // Loop through every level (backwards)
     for (let level = levels; level > 0; level--) {
         // Draw this layer
@@ -38,7 +38,7 @@ function draw() {
         // Set the grey shade of the level based on its number
         // e.g. level 1 will get a shade of 10 (dark gray), 
         // level 10 will be 255(white)
-        const shade = map(level, 1, levels, 10, 255);
+        const shade = map(level, 1, levels, 15, 205);
         // No line around the levels
         noStroke();
         // Set the fill colour to our shade (RGB)
@@ -49,7 +49,7 @@ function draw() {
         // (320, 320) with a size based on the level
         // e.g. level 1 will be a 48x48 rectangle and
         // level 10 will be a 480x480 rectangle
-        rect(320, 320, level * 48, level * 48);
+        rect(320, 350, level * 20, level * 150);
         pop();
     }
 
@@ -57,9 +57,9 @@ function draw() {
     push();
     // No line around the shape
     noStroke();
-    // Make it red (RGB)
-    fill(255, 0, 0);
+    // Make it blue (RGB)
+    fill(0, 0, 210);
     // Draw a 100x100 circle at the mouse position
-    ellipse(mouseX, mouseY, 100, 100);
+    ellipse(mouseX, mouseY, 20, 80);
     pop();
 }
