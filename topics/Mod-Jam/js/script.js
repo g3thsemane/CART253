644309cf8e75f1dd46b080d94bb093c708dd71c9
g3thsemane <saltyravensfan@gmail.com>
2025-10-26 14:45:20 -0400

    Added evil laugh sound to game over
    added a sound that plays when the game is over

--- topics/Mod-Jam/js/script.js
+++ topics/Mod-Jam/js/script.js
@@ -54,6 +54,7 @@ let flyAte;
 let yumYum;
 let rainSong;
 let img1;
+let laughEvil;
 
 //Variable for the score
 let score = 0;
@@ -82,7 +83,9 @@ function preload() {
     //Background song
     rainSong = loadSound('assets/sounds/rainSong.ogg');
     //Image for game over screen
-    img1 = loadImage('assets/thirdcircle.webp');
+    img1 = loadImage('assets/images/thirdcircle.webp');
+    //Evil laugh when game over
+    laughEvil = loadSound('assets/sounds/laughEvil.wav');
 }
 
 function draw() {
@@ -366,6 +369,8 @@ function checkTongueFlyOverlap() {
                 score = 0;
                 gameoverFade = 0;
                 whichScreen = "gameover";
+                rainSong.stop()
+                laughEvil.play()
             }
         }
     }
