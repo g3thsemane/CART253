4f5fcec72c1c6776e0f000e8f139606351b28210
g3thsemane <saltyravensfan@gmail.com>
2025-11-19 16:46:31 -0500

    Trying to debug isssue with original variation not working
    HTML and script for the original variation(Mod Jam) does not seem to be working. Trying to resolve said issue

--- topics/Variation-Jam/DANTE/Dante.js
+++ topics/Variation-Jam/DANTE/Dante.js
@@ -118,15 +118,15 @@ function setup() {
 function preload() {
 
     //Sound when a fly is ate
-    flyAte = loadSound('ASSETS/Sounds/flyAte.wav');
+    flyAte = loadSound('../ASSETS/Sounds/flyAte.wav');
     //Sound frog makes when fly is ate
-    yumYum = loadSound('ASSETS/Sounds/yumYum.mp3');
+    yumYum = loadSound('../ASSETS/Sounds/yumYum.mp3');
     //Background song
-    rainSong = loadSound('ASSETS/Sounds/rainSong.ogg');
+    rainSong = loadSound('../ASSETS/Sounds/rainSong.ogg');
     //Image for game over screen
-    img1 = loadImage('ASSETS/Images/thirdcircle.webp');
+    img1 = loadImage('../ASSETS/Images/thirdcircle.webp');
     //Evil laugh when game over
-    laughEvil = loadSound('ASSETS/Sounds/laughEvil.wav');
+    laughEvil = loadSound('../ASSETS/Sounds/laughEvil.wav');
 }
 
 function draw() {
